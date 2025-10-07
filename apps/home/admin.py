0bf923d343d110129e56ae61bc7dd606844d6026
claymrx571 <claymrx571@gmail.com>
2025-10-07 22:24:16 +0500

    finish

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django_ckeditor_5.widgets import CKEditor5Widget
 from modeltranslation.admin import TranslationAdmin
 from .models import Banner, Home, Messages, Latest, Slide, Slides
 
@@ -10,13 +12,25 @@ class BannerAdmin(TranslationAdmin):
 class HomeAdmin(TranslationAdmin):
     list_display = ['name', 'desc', 'button_name', 'buttons_name', 'buttons_desc',]
 
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditor5Widget(config_name='default')},
+    }
+
 @admin.register(Messages)
 class MessagesAdmin(TranslationAdmin):
     list_display = ['name', 'title', 'desc', 'button',]
 
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditor5Widget(config_name='default')},
+    }
+
 @admin.register(Latest)
 class LatestAdmin(TranslationAdmin):
     list_display = ['name', 'title', 'desc', 'button',]
+
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditor5Widget(config_name='default')},
+    }
 
 @admin.register(Slide)
 class SlideAdmin(TranslationAdmin):
